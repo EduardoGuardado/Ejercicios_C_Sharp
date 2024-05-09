1e1e9bdc511d0bc7ec4a345ffdd2cd70af293d8a
EduardoGuardado <harry11.grd@gmail.com>
2024-05-08 19:18:41 -0600

    Mejorando el código

--- Ejercicios_Variados/OperacionesBasicas.cs
+++ Ejercicios_Variados/OperacionesBasicas.cs
@@ -3,26 +3,32 @@ internal class Program
     private static void Main(string[] args)
     {
         // Variables
-        float num1 = 7;
-        float num2 = 5;
-        float res = 0;
+        float num1 = 7.0f;
+        float num2 = 5.0f;
+        float res = 0.0f;
         // Mostrando las cuatro operaciones matemáticas básicas
         Console.WriteLine("Las cuatro operaciones matemáticas básicas");
         Console.WriteLine($"Números a operar: {num1} y {num2} ");
+        
         Console.WriteLine("** Suma **");
         res = num1 + num2;
-        Console.WriteLine(res + "\n");
+        Console.WriteLine($"{num1} + {num2} = {res} \n");
+        
         Console.WriteLine("** Resta **");
         res = num1 - num2;
-        Console.WriteLine(res + "\n");
+        Console.WriteLine($"{num1} - {num2} = {res} \n");
+        
         Console.WriteLine("** Multiplicación **");
         res = num1 * num2;
-        Console.WriteLine(res + "\n");
+        Console.WriteLine($"{num1} * {num2} = {res} \n");
+        
         Console.WriteLine("** División **");
         res = num1 / num2;
-        Console.WriteLine(res + "\n");
+        Console.WriteLine($"{num1} / {num2} = {res} \n");
+        
         Console.WriteLine("** Módulo **");
         res = num1 % num2;
-        Console.WriteLine(res + "\n");
+        Console.WriteLine($"{num1} % {num2} = {res} \n");
+        Console.WriteLine("**************");
     }
 }
